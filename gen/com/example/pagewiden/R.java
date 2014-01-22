8b004492f7c4315bc2225d8ca5dd9a34852b8f6f
Kartaguez <kartaguez@gmail.com>
2014-01-22 18:35:06 +0100

    Add Qeo components

--- gen/com/example/pagewiden/R.java
+++ gen/com/example/pagewiden/R.java
@@ -9,7 +9,7 @@ package com.example.pagewiden;
 
 public final class R {
     public static final class array {
-        public static final int store_items=0x7f060000;
+        public static final int store_items=0x7f080000;
     }
     public static final class attr {
     }
@@ -20,8 +20,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f070000;
+        public static final int activity_vertical_margin=0x7f070001;
     }
     public static final class drawable {
         public static final int ampoule=0x7f020000;
@@ -170,53 +170,53 @@ public final class R {
         public static final int k=0x7f040000;
     }
     public static final class string {
-        public static final int action_settings=0x7f070001;
-        public static final int app_name=0x7f070000;
-        public static final int barometre_title=0x7f070026;
-        public static final int button_launch_scenario=0x7f070010;
-        public static final int declenchement_objet=0x7f070022;
-        public static final int declenchement_utilisateur=0x7f070021;
-        public static final int download_scenario=0x7f070025;
-        public static final int etat_porte_title=0x7f070029;
-        public static final int list_scenario_title=0x7f07002b;
-        public static final int login_placeholder=0x7f070002;
-        public static final int login_text_button=0x7f070004;
-        public static final int menu_add_scenario=0x7f070011;
-        public static final int no_activ_scenario=0x7f07002e;
-        public static final int password_placeholder=0x7f070003;
-        public static final int peluche_add_scenario=0x7f07002d;
-        public static final int peluche_launch_video=0x7f07002c;
-        public static final int peluche_title=0x7f07002a;
-        public static final int porte_title=0x7f070028;
-        public static final int scenario_actions_titre=0x7f07000e;
-        public static final int scenario_add_title=0x7f070012;
-        public static final int scenario_declencheur_titre=0x7f07000b;
-        public static final int scenario_edit_block_continue_button=0x7f070018;
-        public static final int scenario_edit_block_object_title=0x7f07001f;
-        public static final int scenario_edit_block_title=0x7f07001e;
-        public static final int scenario_edit_block_validation_button=0x7f070017;
-        public static final int scenario_edit_description_placeholder=0x7f070014;
-        public static final int scenario_edit_name_placeholder=0x7f070013;
-        public static final int scenario_edit_name_title=0x7f070019;
-        public static final int scenario_edit_name_validation_button=0x7f070015;
-        public static final int scenario_edit_trigger_title=0x7f07001d;
-        public static final int scenario_edit_trigger_validation_button=0x7f070016;
-        public static final int switch_activation=0x7f07000f;
-        public static final int switch_declencheur=0x7f070020;
-        public static final int title_activity_edition_scenario_block=0x7f07001c;
-        public static final int title_activity_edition_scenario_declencheur=0x7f07001b;
-        public static final int title_activity_edition_scenario_nom=0x7f07001a;
-        public static final int title_activity_main=0x7f070005;
-        public static final int title_activity_scenario_details=0x7f07000a;
-        public static final int title_myobject_detail=0x7f070024;
-        public static final int title_myobject_list=0x7f070023;
-        public static final int title_section1=0x7f070006;
-        public static final int title_section2=0x7f070007;
-        public static final int title_section3=0x7f070008;
-        public static final int trigger_object_title=0x7f07000c;
-        public static final int trigger_param_title=0x7f07000d;
-        public static final int user_name_action=0x7f070009;
-        public static final int webcam_title=0x7f070027;
+        public static final int action_settings=0x7f050001;
+        public static final int app_name=0x7f050000;
+        public static final int barometre_title=0x7f050026;
+        public static final int button_launch_scenario=0x7f050010;
+        public static final int declenchement_objet=0x7f050022;
+        public static final int declenchement_utilisateur=0x7f050021;
+        public static final int download_scenario=0x7f050025;
+        public static final int etat_porte_title=0x7f050029;
+        public static final int list_scenario_title=0x7f05002b;
+        public static final int login_placeholder=0x7f050002;
+        public static final int login_text_button=0x7f050004;
+        public static final int menu_add_scenario=0x7f050011;
+        public static final int no_activ_scenario=0x7f05002e;
+        public static final int password_placeholder=0x7f050003;
+        public static final int peluche_add_scenario=0x7f05002d;
+        public static final int peluche_launch_video=0x7f05002c;
+        public static final int peluche_title=0x7f05002a;
+        public static final int porte_title=0x7f050028;
+        public static final int scenario_actions_titre=0x7f05000e;
+        public static final int scenario_add_title=0x7f050012;
+        public static final int scenario_declencheur_titre=0x7f05000b;
+        public static final int scenario_edit_block_continue_button=0x7f050018;
+        public static final int scenario_edit_block_object_title=0x7f05001f;
+        public static final int scenario_edit_block_title=0x7f05001e;
+        public static final int scenario_edit_block_validation_button=0x7f050017;
+        public static final int scenario_edit_description_placeholder=0x7f050014;
+        public static final int scenario_edit_name_placeholder=0x7f050013;
+        public static final int scenario_edit_name_title=0x7f050019;
+        public static final int scenario_edit_name_validation_button=0x7f050015;
+        public static final int scenario_edit_trigger_title=0x7f05001d;
+        public static final int scenario_edit_trigger_validation_button=0x7f050016;
+        public static final int switch_activation=0x7f05000f;
+        public static final int switch_declencheur=0x7f050020;
+        public static final int title_activity_edition_scenario_block=0x7f05001c;
+        public static final int title_activity_edition_scenario_declencheur=0x7f05001b;
+        public static final int title_activity_edition_scenario_nom=0x7f05001a;
+        public static final int title_activity_main=0x7f050005;
+        public static final int title_activity_scenario_details=0x7f05000a;
+        public static final int title_myobject_detail=0x7f050024;
+        public static final int title_myobject_list=0x7f050023;
+        public static final int title_section1=0x7f050006;
+        public static final int title_section2=0x7f050007;
+        public static final int title_section3=0x7f050008;
+        public static final int trigger_object_title=0x7f05000c;
+        public static final int trigger_param_title=0x7f05000d;
+        public static final int user_name_action=0x7f050009;
+        public static final int webcam_title=0x7f050027;
     }
     public static final class style {
         /** 
@@ -239,11 +239,33 @@ public final class R {
         res/values-v11/styles.xml on API 14+ devices.
     
  API 14 theme customizations can go here. 
+
+        Base application theme, dependent on API level. This theme is replaced
+        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
+    
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
+
+        Base application theme for API 11+. This theme completely replaces
+        AppBaseTheme from res/values/styles.xml on API 11+ devices.
+    
+ API 11 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f080000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
+ Application theme. 
+ All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f080001;
+        public static final int AppTheme=0x7f060001;
     }
 }
